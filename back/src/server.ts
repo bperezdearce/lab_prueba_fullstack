aeaed3f678295d8e9638a9ced680a33f9fb5aaf7
Bárbara Pérez de Arce <barbara.perezdearce@gmail.com>
2025-01-28 16:52:51 -0300

    28/01 5

--- back/src/server.ts
+++ back/src/server.ts
@@ -7,7 +7,13 @@ import { specs } from "./utils/swagger"
 
 const server = express();
 
-server.use(cors({ origin: "*" }));
+server.use(
+    cors({
+      origin: process.env.CORS_ORIGIN || "*",
+      credentials: true,
+      methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    })
+  );
 server.use(morgan("dev"));
 server.use(express.json());
 server.use("/api-docs", SwaggerUi.serve, SwaggerUi.setup(specs));
